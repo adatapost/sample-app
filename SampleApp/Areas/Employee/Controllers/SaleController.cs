35cc82da59d4980e4b58c3cfbef55a7a4e7c6d14
adatapost <adatapost@gmail.com>
2015-02-16 18:25:46 +0530

    Added Employee controller and views

---
+++ SampleApp/Areas/Employee/Controllers/SaleController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace SampleApp.Areas.Employee.Controllers
+{
+    public class SaleController : Controller
+    {
+        // GET: Employee/Sale
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
